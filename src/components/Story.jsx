d0d7026239061468a1aa5c04674e95c7283b5343
Fabricio Roglio de Sousa <locacao@MBL-D449NP9Y40.local>
2025-09-14 14:12:18 -0300

    remove animation from about section

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,50 +1,7 @@
-import gsap from "gsap";
-import { useRef } from "react";
-
 import Button from "./Button";
 import AnimatedTitle from "./AnimatedTitle";
 
 const FloatingImage = () => {
-  const frameRef = useRef(null);
-
-  const handleMouseMove = (e) => {
-    const { clientX, clientY } = e;
-    const element = frameRef.current;
-
-    if (!element) return;
-
-    const rect = element.getBoundingClientRect();
-    const xPos = clientX - rect.left;
-    const yPos = clientY - rect.top;
-
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-
-    const rotateX = ((yPos - centerY) / centerY) * -10;
-    const rotateY = ((xPos - centerX) / centerX) * 10;
-
-    gsap.to(element, {
-      duration: 0.3,
-      rotateX,
-      rotateY,
-      transformPerspective: 500,
-      ease: "power1.inOut",
-    });
-  };
-
-  const handleMouseLeave = () => {
-    const element = frameRef.current;
-
-    if (element) {
-      gsap.to(element, {
-        duration: 0.3,
-        rotateX: 0,
-        rotateY: 0,
-        ease: "power1.inOut",
-      });
-    }
-  };
-
   return (
     <div id="story" className="min-h-dvh w-screen bg-blue-400 text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
@@ -62,11 +19,6 @@ const FloatingImage = () => {
             <div className="story-img-mask">
               <div className="story-img-content">
                 <img
-                  ref={frameRef}
-                  onMouseMove={handleMouseMove}
-                  onMouseLeave={handleMouseLeave}
-                  onMouseUp={handleMouseLeave}
-                  onMouseEnter={handleMouseLeave}
                   src="/img/entrance.webp"
                   alt="entrance.webp"
                   className="object-contain"
